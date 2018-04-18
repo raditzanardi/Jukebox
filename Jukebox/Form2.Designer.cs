f9cf17658650efbad60a94747a016f8faaca914d
Radit Zanardi <radit268@outlook.com>
2018-04-18 12:13:44 +0100

    Commit

--- Jukebox/Form2.Designer.cs
+++ Jukebox/Form2.Designer.cs
@@ -44,7 +44,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(243, 31);
             this.label1.TabIndex = 0;
-            this.label1.Text = "My Juke Box v2.0";
+            this.label1.Text = "My Juke Box v1.0";
             // 
             // textBox1
             // 
